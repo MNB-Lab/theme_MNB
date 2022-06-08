8e9c8cf048d2257c7032ccf4e7d5c9d11d9c9073
Nirmal <k.nirmalraman@gmail.com>
2022-06-08 10:47:21 +0200

    Create sourceThemeMNB.r
    added a wrapper function to source all theme related functions

---
+++ sourceThemeMNB.r
@@ -0,0 +1,13 @@
+## A wrapper function to source all theme related functions
+## Authors: Nirmal Kannaiyan, Rajinder Gupta
+## Date: 08.06.2022
+
+
+sourceThemeMNB <- function() {
+
+source("https://raw.githubusercontent.com/MNB-Lab/theme_MNB/main/ggAnyCaseLables.r")
+source("https://raw.githubusercontent.com/MNB-Lab/theme_MNB/main/plotTheme.r")
+
+}
+
+sourceThemeMNB()
